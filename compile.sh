ede760d8bfa24833dc9f793db49e3a340d5d2048
Lennart Vogelsang <lennart@vogelsang.berlin>
2019-11-25 22:34:47 +0100

    added smart pandoc extension for better quotation marks and more

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 infile=$(ls | grep ".md$" | grep -v "README.md" | sort -n)
-pandoc $infile -o ${infile%.*}.pdf --from markdown --template "eisvogel.latex" --listings --number-sections --filter pandoc-crossref --filter pandoc-citeproc
+pandoc $infile -o ${infile%.*}.pdf -f markdown+smart --from markdown --template "eisvogel.latex" --listings --number-sections --filter pandoc-crossref --filter pandoc-citeproc
